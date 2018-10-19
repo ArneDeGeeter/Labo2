652e78bc1638e8f9f543b15efd40ae2c1391d3dd
Arne <arne.de.geeter0@gmail.com>
2018-10-19 11:19:37 +0200

    test
    test

--- src/be/kul/gantry/domain/Main.java
+++ src/be/kul/gantry/domain/Main.java
@@ -40,6 +40,8 @@ public class Main {
             System.out.println();
         }*/
         System.out.println();
+        System.out.println();
+        System.out.println();
         System.out.println(prob.getInputJobSequence());
         System.out.println(prob.getGantries());
     }
